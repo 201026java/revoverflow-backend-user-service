8c106f823a6903c06aeae87f3d4d1b08a2841558
Mark Alsip <mark.alsip@comcast.net>
2020-11-06 10:28:22 -0700

    added data.sql data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (user_id,account_id, points, admin_role, profile_picture, email, first_name, last_name) 
+VALUES (13, 15, 100, false, NULL, 'user@rss.com', 'User', 'User'), 
+(12, 26, 20, true, NULL, 'admin@rss.com', 'Admin', 'Admin'),
+(14, 26, 20, true, NULL, 'testing@rss.com', 'Admin', 'Admin');
