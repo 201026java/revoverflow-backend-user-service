abead6ab762e4a19857f93610752523d3eabf843
Mark Alsip <mark.alsip@comcast.net>
2020-11-06 13:53:52 -0700

    separated service from controller in preparation for feign and messaging communication

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,45 +1,39 @@
 package com.revature.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.User;
-import com.revature.repositories.UserRepository;
+import com.revature.services.UserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(
+		origins = { "http://localhost:8500" }, 
+		methods = { RequestMethod.GET, RequestMethod.PUT, 
+					RequestMethod.PATCH, RequestMethod.POST },
+		allowedHeaders = { "content-type" }
+	)
 public class UserController {
 
 	@Autowired
-	private UserRepository userRepository;
-
-	@GetMapping("/{id}")
-	public ResponseEntity<User> getById(@PathVariable int id){
-		Optional<User> user = userRepository.findById(id);
-
-		if(user.isPresent()) {
-			return ResponseEntity.ok(user.get());			
-		}
-
-		return ResponseEntity.noContent().build();
-	}
+	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<List<User>> getAllUser(){
-		List<User> users = userRepository.findAll();
-
-		if(users.size() != 0) {
-			System.out.print(users.size());
-			return ResponseEntity.ok(users);		
-		}
-
-		return ResponseEntity.noContent().build();
+	public List<User> getAllUser(){
+		return userService.getAllUser();
 	}
+	
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable int id) {
+		return userService.getUserById(id);
+	}
+
 }
