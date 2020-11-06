3c29d0122757852392735e1f45ae4a2a13a88cf0
Mark Alsip <mark.alsip@comcast.net>
2020-11-06 13:53:52 -0700

    separated service from controller in preparation for feign and messaging communication

---
+++ src/main/java/com/revature/services/UserService.java
@@ -0,0 +1,81 @@
+package com.revature.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.revature.models.User;
+import com.revature.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+//	@Autowired
+//	RSSService rssService;
+	
+	@GetMapping
+	public List<User> getAllUser(){
+		List<User> users = userRepository.findAll();
+
+		if(users.size() != 0) {
+			System.out.print(users.size());
+			return users;		
+		}
+
+		return null;
+	}
+	
+	/**
+	 * @author Ryan Clayton
+	 * @param id this is the user's Id
+	 * @return User this returns the user entity with updated RSS points
+	 */
+
+	public User getUserById(int id) {
+			try {
+				Optional<User> optUser = userRepository.findById(id);
+				if(optUser.isPresent()) {
+					return optUser.get();
+				
+					//user.setPoints(rssService.getPoints(id));
+					//return userRepository.save(user);
+				}
+			}catch(Exception e) {
+
+				//Logger logger = Logger.getLogger(UserService.class);
+				//logger.error("error in UserService getUserById", e);
+				return null;
+			}
+			return null;
+	}
+	
+	
+//	public Collection<GrantedAuthority> getAuthority(User u){
+//		Collection<GrantedAuthority>auths = new ArrayList<>();
+//		Optional<User> optUser = userRepository.findById(u.getUserID());
+//		SimpleGrantedAuthority a= null; 
+//		
+//		if (optUser.isPresent()) {
+//			User user = optUser.get();
+//			if(user.isAdmin()) {
+//				a = new SimpleGrantedAuthority("admin");
+//				auths.add(a);
+//				a = new SimpleGrantedAuthority("user");
+//				auths.add(a);
+//			}else {
+//				a = new SimpleGrantedAuthority("user");
+//				auths.add(a);
+//				
+//			}
+//		}
+//		return auths;
+//		
+//	}
+
+}
