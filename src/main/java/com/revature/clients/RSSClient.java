c71ab8774f40bd505b7a07bd62f88326fee4859d
bukadirit <bukadiri.trawally@mail.citytech.cuny.edu>
2020-11-09 04:03:25 -0500

    Added logic in the userService to save new user along with their rssAccount id and point amount. Modified RSSClient to accomodate more than 1 external endpoint.

--- src/main/java/com/revature/clients/RSSClient.java
+++ src/main/java/com/revature/clients/RSSClient.java
@@ -1,13 +1,19 @@
 package com.revature.clients;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.revature.DTOs.RSSAccountDTO;
 import com.revature.DTOs.RSSUserDTO;
 
-@FeignClient(name = "Accounts-Service", url = "${environment.rss.url}")
+@FeignClient(name = "Accounts-Service", url = "${environment.rss.login.url}")
 public interface RSSClient {
 
-	@PostMapping("/login")
+	@PostMapping("/user/login")
 	public RSSUserDTO loginUser(RSSUserDTO user);
+	
+	@PostMapping("/account/accounts")
+	public List<RSSAccountDTO> findAccountByUserId(RSSUserDTO user);
 }
