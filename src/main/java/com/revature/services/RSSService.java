eff3d06a0e90e85ce6eb4b134f313cb92b4da133
bukadirit <bukadiri.trawally@mail.citytech.cuny.edu>
2020-11-09 01:35:21 -0500

    Created feign client to retrieve user information from the RSS service. Add environmental varible for the RSS sevice url in the application.properties file.

--- src/main/java/com/revature/services/RSSService.java
+++ src/main/java/com/revature/services/RSSService.java
@@ -2,8 +2,13 @@ package com.revature.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.revature.DTOs.RSSUserDTO;
+import com.revature.clients.RSSClient;
+import com.revature.models.User;
 import com.revature.repositories.UserRepository;
 
 @Service
@@ -11,22 +16,31 @@ public class RSSService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	@Autowired private CircuitBreakerFactory<?, ?> cbFactory;
-	 
-	/*
-	 * public ResponseEntity<User> login(RSSUserDTO user) { User auth = new User();
-	 * 
-	 * return this.cbFactory.create("login").run( // Positive Case () -> {
-	 * RSSUserDTO u = rssClient.getLogin(user); auth.setUserID(u.getUserId());
-	 * return ResponseEntity.ok(auth); },
-	 * 
-	 * // Negative Case throwable -> loginFallback()); }
-	 * 
-	 * public ResponseEntity<User> loginFallback() { return
-	 * ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(null); }
-	 */
-	 
-	
-}
 
+	@Autowired
+	private CircuitBreakerFactory<?, ?> cbFactory;
+	
+	@Autowired
+	private RSSClient rssClient;
+
+	
+	public ResponseEntity<User> login(RSSUserDTO user) { 
+
+		return this.cbFactory.create("login").run( 
+			() -> {
+					RSSUserDTO u = rssClient.loginUser(user); 
+					if(u == null) {
+						return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+					}
+					User auth = new User(u.getUserId(), 0, 0, u.isAdmin(), null, u.getEmail(), u.getFirstName(), u.getLastName());
+					return ResponseEntity.ok(auth); 
+				},
+				throwable -> loginFallback());
+}
+	  
+	 public ResponseEntity<User> loginFallback() { 
+		 return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(null); 
+	 }
+	 
+
+}
