58925f96b48c39e16580a0b3cad42e9dcac7d02f
bukadirit <bukadiri.trawally@mail.citytech.cuny.edu>
2020-11-13 15:09:59 -0500

    Testing security between services

---
+++ src/main/java/com/revature/security/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.revature.security;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
+
+@EnableWebSecurity 	// Enable security config. This annotation denotes config for spring security.
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+ 	@Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+ 		httpSecurity.headers().frameOptions().disable();
+        httpSecurity.cors()
+        	//disable csrf and allow these specific endpoints
+        	.and().csrf().disable().authorizeRequests().antMatchers("/user/login", "/user/authorize", "/user/roles", "/actuator/health", "/h2-console/**").permitAll().anyRequest().authenticated()
+        	.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        	  // handle an authorized attempts 
+        	.and().exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+ 	}
+ 	
+ 	@Bean
+    public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+        DefaultHttpFirewall firewall = new DefaultHttpFirewall();
+        firewall.setAllowUrlEncodedSlash(true);
+        return firewall;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
+    }
+}
