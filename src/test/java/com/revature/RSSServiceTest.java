5cdfeb58292814ba73076e79db4bd6a319537578
bukadirit <bukadiri.trawally@mail.citytech.cuny.edu>
2020-11-17 14:35:37 -0500

    Fixed all failing tests

---
+++ src/test/java/com/revature/RSSServiceTest.java
@@ -0,0 +1,66 @@
+package com.revature;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.revature.models.User;
+import com.revature.repositories.UserRepository;
+import com.revature.services.RSSService;
+
+@RunWith(JUnit4.class)
+public class RSSServiceTest {
+	static User u1, u2;
+
+	@Mock
+	UserRepository userRepository;
+	
+	@InjectMocks
+	RSSService rssService;
+	
+
+	@BeforeEach
+	public void setup() {		
+		u1 = (new User(12,26,0,true,null,"admin@rss.com","Admin","Admin", null));
+		u2 = (new User(13,26,0,false,null,"user@rss.com","User","User", null));
+		
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void test_granted_authorities_for_admin_will_return_adminAndUserRoles() {
+		Mockito.when(userRepository.findById(u1.getUserID())).thenReturn(Optional.of(u1));
+		Collection<GrantedAuthority> adminRoles = new ArrayList<GrantedAuthority>();
+		SimpleGrantedAuthority a= null; 
+		a = new SimpleGrantedAuthority("ADMIN");
+		adminRoles.add(a);
+		a = new SimpleGrantedAuthority("USER");
+		adminRoles.add(a);
+		
+		Collection<? extends GrantedAuthority> result = rssService.getAuthority(u1);
+		assertEquals(adminRoles, result);
+	}
+	
+	@Test public void test_granted_authorities_user_will_return_just_userRole() {
+		Collection<GrantedAuthority> userRoles = new ArrayList<GrantedAuthority>();
+		SimpleGrantedAuthority b= null; b = new SimpleGrantedAuthority("USER");
+		userRoles.add(b);
+		  
+		Mockito.when(userRepository.findById(u2.getUserID())).thenReturn(Optional.of(u2)); 
+		Collection<? extends GrantedAuthority> result = rssService.getAuthority(u2); assertEquals(userRoles, result); 
+	
+	}
+}
--- src/test/java/com/revature/UserControllerTest.java
+++ src/test/java/com/revature/UserControllerTest.java
@@ -1,7 +1,9 @@
 package com.revature;
 
 import static org.junit.Assert.assertEquals;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -15,6 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.revature.models.User;
@@ -22,74 +25,66 @@ import com.revature.services.UserService;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration
-@SpringBootTest(
-		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-		classes = UserServiceApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = UserServiceApplication.class)
 @AutoConfigureMockMvc
 public class UserControllerTest {
 	static int id = 12;
 	static User u1;
-	
+
 	@Autowired
 	private WebApplicationContext context;
-	
-	@Mock                           
-	private UserService userService; 
-	
-	@Autowired                           
-    private MockMvc mockMvc;  
-                                         
-    
-//    @Before                          
-//    public void setUp() {  
-//       u1 = new User(12,26,0,true,null,"admin@rss.com","Admin","Admin");
-//   	   mockMvc = MockMvcBuilders
-//   				.webAppContextSetup(context)
-//   				.apply(springSecurity())
-//   				.build();
-//    }
-	
-    @Test
-//    @WithMockUser(username = "user@rss.com", password = "Password123!", authorities = "user")
-    public void will_return_user_with_a_status_of_200() throws Exception {
-    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/user/12")
-    			.accept(MediaType.APPLICATION_JSON)
-    			).andReturn();
-    			String content = result.getResponse().getContentAsString();
-    			assertEquals(200, result.getResponse().getStatus());
-    			//System.out.println(content);
+
+	@Mock
+	private UserService userService;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	public void setUp() {
+		u1 = new User(12, 26, 0, true, null, "admin@rss.com", "Admin", "Admin", null);
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+	}
+
+	@Test
+	@WithMockUser(username = "user@rss.com", password = "12345", authorities = "USER")
+	public void will_return_user_with_a_status_of_200() throws Exception {
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/user/12").accept(MediaType.APPLICATION_JSON))
+				.andReturn();
+		String content = result.getResponse().getContentAsString();
+		assertEquals(200, result.getResponse().getStatus());
+		// System.out.println(content);
 //    			assertTrue("This return object conains the string", content.contains("admin@rss.com"));
-    }
-    
-    @Test
-//    @WithMockUser(username = "user@rss.com", password = "Password123!", authorities = "user")
-    public void will__not_return_user_with_a_status_of_200() throws Exception {
-    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/user/15")
-    			.accept(MediaType.APPLICATION_JSON)
-    			).andReturn();
-    			String content = result.getResponse().getContentAsString();
-    			assertEquals(200, result.getResponse().getStatus());
-    			assertEquals("", content);
-    }
-    @Test
-    @WithMockUser(username = "user@rss.com", password = "Password123!", authorities = "user")
-    public void will_return_nothing_with_a_status_of_400() throws Exception {
-    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/user/alatreon")
-    			.accept(MediaType.APPLICATION_JSON)
-    			).andReturn();
-    			String content = result.getResponse().getContentAsString();
-    			assertEquals(400, result.getResponse().getStatus());
-    			assertEquals("", content);
-    }
-    
-    @Test
-    @WithMockUser(username = "user@rss.com", password = "Password123!", authorities = "user")
-    public void will_return_nothing_with_a_status_of_404() throws Exception {
-    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/user/20/alatreon")
-    			.accept(MediaType.APPLICATION_JSON)
-    			).andReturn();
-    			String content = result.getResponse().getContentAsString();
-    			assertEquals(404, result.getResponse().getStatus());
-    			assertEquals("", content);
-    }
+	}
+
+	
+	 @Test
+	 @WithMockUser(username = "user@rss.com", password = "12345", authorities = "USER") 
+	 public void will__not_return_user_with_a_status_of_200() throws Exception { MvcResult
+		 result = mockMvc.perform(MockMvcRequestBuilders.get("/user/15")
+		 .accept(MediaType.APPLICATION_JSON) ).andReturn(); String content =
+		 result.getResponse().getContentAsString(); assertEquals(200,
+		 result.getResponse().getStatus()); assertEquals("", content); 
+		 }
+	 
+	@Test
+	@WithMockUser(username = "user@rss.com", password = "Password123!", authorities = "user")
+	public void will_return_nothing_with_a_status_of_400() throws Exception {
+		MvcResult result = mockMvc
+				.perform(MockMvcRequestBuilders.get("/user/alatreon").accept(MediaType.APPLICATION_JSON)).andReturn();
+		String content = result.getResponse().getContentAsString();
+		assertEquals(400, result.getResponse().getStatus());
+		assertEquals("", content);
+	}
+
+	@Test
+	@WithMockUser(username = "user@rss.com", password = "Password123!", authorities = "user")
+	public void will_return_nothing_with_a_status_of_404() throws Exception {
+		MvcResult result = mockMvc
+				.perform(MockMvcRequestBuilders.get("/user/20/alatreon").accept(MediaType.APPLICATION_JSON))
+				.andReturn();
+		String content = result.getResponse().getContentAsString();
+		assertEquals(404, result.getResponse().getStatus());
+		assertEquals("", content);
+	}
 }
